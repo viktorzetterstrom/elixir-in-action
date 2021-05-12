d744385a2e0f60033d8c1a2026c48c6ddb864a70
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-05-12 08:58:25 +0200

    add struct to TodoList

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,11 +1,32 @@
 defmodule TodoList do
-  def new(), do: MultiDict.new()
+  defstruct auto_id: 1, entries: %{}
 
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  def new(), do: %TodoList{}
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
+  end
+
+  def update_entry(todo_list, entry_id, updater_lambda) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_lambda.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
   end
 
   def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
   end
 end
